dda1d3b203fa9f2f8045f0fcf8ef71602f6f6c9e
Tim Holzheim <holzheim@dbis.rwth-aachen.de>
2025-05-06 12:05:35 +0200

    applies formatting

--- tests/api/test_get_volume_paper_ids.py
+++ tests/api/test_get_volume_paper_ids.py
@@ -19,9 +19,9 @@ def test_get_volume_paper_ids():
 def test_get_volume_id():
     volume_number = 3450
     expected_qid = "https://ceur-dev.wikibase.cloud/entity/Q8940"
-    expected_documents = 6
     volume_qid = client.get(f"/ceur-ws/Vol-{volume_number}").json()
     assert expected_qid == volume_qid
+
 
 if __name__ == "__main__":
     unittest.main()
